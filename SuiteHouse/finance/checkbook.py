11fbcbbbf201da34eec5f204fd7e2f2c59769e13
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-10 08:13:18 -0400

    Began working on data modeling, not sure about the checkbook modeling right now. I think I need to think about it a little more and decide whether or not there should be a checkbook per user, or just associated checkbook items to a user. To perform operations on the data it makes sense to have both checkbook and checkbookitems data. simplicity sake should have just checkbookitems but that seems inefficient if the userbase were large.

--- SuiteHouse/finance/checkbook.py
+++ SuiteHouse/finance/checkbook.py
@@ -1,6 +1,9 @@
 #Import google sign in
 from google.appengine.api import users
 
+#database modeling
+from google.appengine.ext import ndb
+
 import webapp2
 import jinja2
 
@@ -13,17 +16,42 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'])
 
 
+class CheckBookItem(ndb.Model):
+	"""Model of a checkbook item"""
+	description = ndb.StringProperty(indexed=False)
+	date = ndb.DateProperty(auto_now_add=True)
+	amount = ndb.FloatProperty()
+	checkbook = ndb.IntegerProperty() #checkbook key
+
+class CheckBookModel(ndb.Model):
+	"""Model of a checkbook, has a one to many relationship with CheckBookItems and a one to one to users"""
+	associated_user = ndb.IntegerProperty() #user_id of user
+	totalIncome = ndb.FloatProperty()
+	totalExpenses = ndb.FloatProperty()
+
+
+
+
 class CheckBook(webapp2.RequestHandler):
 
 	def get(self):
 		user = users.get_current_user()
 
-		if not user:
+		if user:
+			#Get the checkbook associated with this user
+			checkbook = CheckBookModel.query(ancestor=ndb.Key('AssociatedUser',user.user_id())).fetch(1)
+			
+			template_values = {
+				'username': user.nickname(),
+			}
+
+			template = JINJA_ENVIRONMENT.get_template('checkbook.html')
+			self.response.write(template.render(template_values))
+		else:
 			self.redirect(users.create_login_url(self.request.uri))
 
-		template_values = {
-			'username': user.nickname(),
-		}
-
-		template = JINJA_ENVIRONMENT.get_template('checkbook.html')
-		self.response.write(template.render(template_values))
+	def post(self):
+		user = users.get_current_user()
+		checkbook = CheckBookModel(parent=ndb.Key('AssociatedUser',user.user_id()))
+		self.redirect('/finance/checkbook') #Probably want to pass some parameters to the url about success or not sucesss
+		
--- SuiteHouse/index.py
+++ SuiteHouse/index.py
@@ -1,17 +1,25 @@
 #Import google sign in
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 import webapp2
 import jinja2
 
+
 #Import os for finding jinja
 import os
 from finance.finance import FinancePage
+from finance.checkbook import CheckBook
 
 #Make sure to setup the template rendering evironment in the templates directory
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates/'),
     extensions=['jinja2.ext.autoescape'])
+
+class User(ndb.Model):
+	"""User model for the datastore to hold so we can have a single user object to deal with"""
+	user_id = ndb.StringProperty() #Store the user_id() because it won't ever change
+
 
 class LandingPage(webapp2.RequestHandler):
 
@@ -19,8 +27,10 @@ class LandingPage(webapp2.RequestHandler):
 		user = users.get_current_user()
 
 		if user:
+			#Store the user information if they don't exist already 
+
 			template_values = {
-				'username': user.nickname(),
+				'username': user.user_id(),
 			}
 			template = JINJA_ENVIRONMENT.get_template('index.html')
 			self.response.write(template.render(template_values))
