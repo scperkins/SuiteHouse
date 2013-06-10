5df7673682667979148f335f893cb172534bf185
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-09 23:18:27 -0400

    Added SuiteHouse folder to project, it contains the app engine version of the application. After Seans approval, we'll move the application to primarily using the app engine only, this will help clean the repository up a bit I think

---
+++ SuiteHouse/finance/finance.py
@@ -0,0 +1,36 @@
+#Import google sign in
+from google.appengine.api import users
+
+import webapp2
+import jinja2
+
+#Import os for finding jinja
+import os
+
+import checkbook
+
+#Make sure to setup the template rendering evironment in the templates directory
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates/'),
+    extensions=['jinja2.ext.autoescape'])
+
+class FinancePage(webapp2.RequestHandler):
+
+	def get(self):
+		user = users.get_current_user()
+
+		if not user:
+			self.redirect(users.create_login_url(self.request.uri))
+
+		template_values = {
+			'username': user.nickname(),
+		}
+
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+
+
+application = webapp2.WSGIApplication([
+    ('/finance',FinancePage),
+    ('/finance/checkbook',checkbook.CheckBook),
+], debug=True)
